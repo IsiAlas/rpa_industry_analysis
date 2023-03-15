542322a136fc877f7f80eb8f168b41d1a6af78f6
isi <48052532+IsiAlas@users.noreply.github.com>
2023-03-15 13:30:17 -0300

    Add files via upload

---
+++ pages/8_ℹ️ Sources.py
@@ -0,0 +1,47 @@
+import streamlit as st
+
+
+with st.container():
+    st.markdown("<h3 style='text-align: center; font-size:40px; color:#EE103F \
+'>Sources</h3>", unsafe_allow_html=True)
+    st.write("#")
+
+st.markdown("**:red[RPA Definition]**")
+st.markdown('''''')
+st.markdown('''
+            - [What is RPA Software? - Gartner](https://www.gartner.com/reviews/market/robotic-process-automation-software)
+            - [What is Robotic Process Automation (RPA)?](https://www.automationanywhere.com/rpa/robotic-process-automation)
+            - [RPA (Robotic Process Automation)](https://digitalworkforce.com/rpa-robotic-process-automation/)
+            - [What is robotic process automation (RPA)?](https://www.ibm.com/topics/rpa)
+            ''')
+st.markdown("**:red[Market Information]**")
+st.markdown('''
+            - [RPA Market Research - Fortune Business Insights](https://www.fortunebusinessinsights.com/robotic-process-automation-rpa-market-102042)
+            - [RPA Market Research -  Global Newswire](https://www.globenewswire.com/en/news-release/2022/09/27/2523189/0/en/Robotic-Process-Automation-Market-Size-2022-2029-Worth-USD-43-52-Billion-Exhibiting-a-CAGR-of-23-4.html)
+            - [The Forrester Wave RPA](https://www.uipath.com/es/resources/automation-analyst-reports/forrester-wave-rpa)
+            ''')
+st.markdown("**:red[Pricing, Revenue & Valuation]**")
+st.markdown('''
+            - [RPA Pricing: Comparison of Leading RPA Vendors’ Fees in 2023](https://research.aimultiple.com/rpa-pricing/)
+            - [California-based Automation Anywhere raises $200 million](https://research.aimultiple.com/rpa-pricing/)
+            - [Getting to the bottom of UiPath’s plunging valuation](https://techcrunch.com/2022/05/01/getting-to-the-bottom-of-uipaths-plunging-valuation/?guccounter=1&guce_referrer=aHR0cHM6Ly93d3cuZ29vZ2xlLmNvbS8&guce_referrer_sig=AQAAAK2-UHbIm06Pb9z0l_caPuoyWFACr0LvjdrQik50m8VJM68g_OC15bK1jsCRSS0X55lR4fzJO9jGXiTQDote4z6Cpg7zUlW1ad5ApHVjaFEyz7RLM9-3Gpv1Zrk968TEWTyJurf3eq25RNuEzEKlD1x7t6e0ZhZ2jZGXBBew3k8I)
+            - [UiPath Revenue 2021-2022](https://www.macrotrends.net/stocks/charts/PATH/uipath/revenue#:~:text=UiPath%20revenue%20for%20the%20twelve,a%2080.76%25%20increase%20from%202020.)
+            - [Blue Prism Technology Service Market](https://www.factmr.com/report/2239/blue-prism-technology-services-market)
+            - [WorkFusion Revenue and Competitors](https://growjo.com/company/WorkFusion#:~:text=Estimated%20Revenue%20%26%20Valuation&text=WorkFusion%27s%20total%20funding%20is%20%24341.3M.)
+            - [The Economic Impact of UiPath Robotic Process Automation: Key Takeaways](https://smartbridge.com/the-economic-impact-of-uipath-robotic-process-automation-key-takeaways/)
+            - [Blue Prism - Dealroom](https://app.dealroom.co/companies/blue_prism_group_plc)
+            - [UiPath - Dealroom](https://app.dealroom.co/companies/uipath)
+            - [NICE Systems - Dealroom](https://app.dealroom.co/companies/nice_systems)
+            - [WorkFusion - Dealroom](https://app.dealroom.co/companies/workfusion)
+            - [Cyclone- Dealroom](https://app.dealroom.co/companies/cyclone_1)
+            - [Nintex](https://app.dealroom.co/companies/nintex)
+            - [Laiye](https://app.dealroom.co/companies/laiye)
+            - [Pegasystems](https://app.dealroom.co/companies/pegasystems)
+            - [Appian](https://app.dealroom.co/companies/appian)
+''')
+st.markdown("**:red[Other]**")
+st.markdown('''
+            - [The History of RPA Automation](https://adaptivegrowth.com/the-history-of-robotic-process-automation/#:~:text=The%20history%20of%20RPA%20dates,systems%20and%20incompatible%20legacy%20systems.)
+            - [Classifying businesses by size](https://www.open.edu/openlearn/money-business/business-strategy-studies/different-types-business/content-section-2)
+
+            ''')
